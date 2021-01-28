bb2c367310ec1dc83cdeefbd29fabc1f309fbfa1
Daniel Tavares <57456238+DanielTavares33@users.noreply.github.com>
2021-01-28 18:00:50 +0000

    PageHeader Component
    Creation of the PageHeader Component

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import PageHeader from '../template/pageHeader'
+
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name="Sobre" small="Nós" />
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <h2>Impresa</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     </div>
 )
