51ea7e19d01c85e3aef44b6580ef58b1e2d1dfce
Daniel Tavares <57456238+DanielTavares33@users.noreply.github.com>
2021-03-25 20:14:22 +0000

    Action Creator changeDescription implementation

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,22 +1,8 @@
 import { combineReducers } from 'redux'
+import todoReducer from '../todo/todoReducer'
 
 const rootReducer = combineReducers({
-    todo: () => ({
-        description: 'Ler livro',
-        list: [{
-            _id: 1,
-            description: 'Pagar fatura do cartão',
-            done: true
-        }, {
-            _id: 2,
-            description: 'Reunião com a equipa às 10h',
-            done: false
-        }, {
-            _id: 4,
-            description: 'Consulta médica na terça depois do almoço',
-            done: false
-        }]
-    })
+    todo: todoReducer
 })
 
 export default rootReducer
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-const todoForm = props => {
+//ACTIONS
+import { changeDescription } from './todoActions'
+
+const TodoForm = props => {
 
     const keyHandler = (e) => {
         if (e.key === 'Enter') {
@@ -18,7 +22,7 @@ const todoForm = props => {
         <div role="form" className="todoForm">
             <Grid cols='12 9 10'>
                 <input id="description" className="form-control" placeholder="Adicione uma tarefa"
-                    value={props.description} onChange={props.handleChange} onKeyUp={keyHandler} />
+                    value={props.description} onChange={props.changeDescription} onKeyUp={keyHandler} />
             </Grid>
             <Grid cols='12 3 2'>
                 <IconButton style="primary" icon="plus" onClick={props.handleAdd} title="Enter" />
@@ -32,4 +36,7 @@ const todoForm = props => {
 const mapStateToProps = state => ({
     description: state.todo.description
 })
-export default connect(mapStateToProps)(todoForm)
+
+const mapDispatchToProps = dispatch => bindActionCreators({ changeDescription }, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
