b3805a63fd5800dbf80cfbadf6fc84d20bca7c0b
Daniel Tavares <57456238+DanielTavares33@users.noreply.github.com>
2021-03-27 18:58:17 +0000

    Applying Middleware (Redux-Promise)

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,18 +1,6 @@
 const INITIAL_STATE = {
-    description: 'Ler livro',
-    list: [{
-        _id: 1,
-        description: 'Pagar fatura do cartão',
-        done: true
-    }, {
-        _id: 2,
-        description: 'Reunião com a equipa às 10h',
-        done: false
-    }, {
-        _id: 4,
-        description: 'Consulta médica na terça depois do almoço',
-        done: false
-    }]
+    description: '',
+    list: []
 }
 
 export default (state = INITIAL_STATE, action) => {
