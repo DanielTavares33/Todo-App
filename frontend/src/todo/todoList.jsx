1d6d071596334c190635e62af9e2f25fd6c0225e
Daniel Tavares <57456238+DanielTavares33@users.noreply.github.com>
2021-03-30 19:11:14 +0100

    Refactoring markAsDone, markAsPending and remove with redux-thunk

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux' // connect this componet with the data from the store and actions
+import { bindActionCreators } from 'redux'
+import { markAsDone, markAsPending, remove } from './todoActions'
+
 import IconButton from "../template/iconButton"
 
 const TodoList = props => {
@@ -12,9 +15,9 @@ const TodoList = props => {
             <tr key={todo._id}>
                 <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
-                    <IconButton style="success" icon="check" hide={todo.done} onClick={() => props.handleMarkAsDone(todo)} />
-                    <IconButton style="warning" icon="undo" hide={!todo.done} onClick={() => props.handleMarkAsPending(todo)} />
-                    <IconButton style="danger" icon="trash-o" hide={!todo.done} onClick={() => props.handleRemove(todo)} />
+                    <IconButton style="success" icon="check" hide={todo.done} onClick={() => props.markAsDone(todo)} />
+                    <IconButton style="warning" icon="undo" hide={!todo.done} onClick={() => props.markAsPending(todo)} />
+                    <IconButton style="danger" icon="trash-o" hide={!todo.done} onClick={() => props.remove(todo)} />
                 </td>
             </tr>
         ))
@@ -39,4 +42,6 @@ const mapStateToProps = state => ({
     list: state.todo.list //comes from :: "./main/reducers"
 })
 
-export default connect(mapStateToProps)(TodoList)
+const mapDispatchToProps = dispatch => bindActionCreators({ markAsDone, markAsPending, remove }, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
